c246376136add39d2a79430bdc9df5891ca4c9ad
Andrey Shor <andreyshor@Andreys-iMac.local>
2019-01-11 17:54:22 -0500

    Initial Commit

---
+++ Tipper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipper
+//
+//  Created by Andrey Shor on 1/11/19.
+//  Copyright © 2019 Andrey Shor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
